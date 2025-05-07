2e65e74d465c8c39a50d8f6bc5768dd6a04e28d4
lamtota40 <lamtota40@gmail.com>
2025-05-07 12:20:19 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -10,12 +10,18 @@ EOF
 clear
 
 
-
+#resize/mengecilkan partisi
 parted /dev/vda ---pretend-input-tty <<EOF
 resizepart 2 20GB
 yes
 quit
 EOF
+
+#membuat partisi baru
 parted /dev/vda --script mkpart primary btrfs 20GB 100%
+#refresh ulang sistem agar mengenali partisi baru
 partprobe /dev/vda
 #cek lsblk akan muncul partisi baru /dev/vda3
+
+#format partisi
+mkfs.btrfs -f -L dataku /dev/vda3
