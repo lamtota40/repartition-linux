25182feb442a0e1bc4ec9ee4fc356e3d022510b7
lamtota40 <lamtota40@gmail.com>
2025-05-07 15:14:47 +0700

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -2,3 +2,8 @@ parted /dev/vda --script mklabel msdos
 parted /dev/vda --script mkpart primary ext4 1MiB 513MiB
 parted /dev/vda --script mkpart primary btrfs 513MiB 30GB
 parted /dev/vda --script mkpart primary btrfs 30GB 100%
+
+
+mkfs.ext4 /dev/vda1 -L boot
+mkfs.btrfs -f -L system /dev/vda2
+mkfs.btrfs -f -L data /dev/vda3
