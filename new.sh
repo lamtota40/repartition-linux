575bac0f16347842e1a87abfa732a112f862c383
lamtota40 <lamtota40@gmail.com>
2025-05-07 12:27:51 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,27 +1,22 @@
-lsblk
-parted /dev/vda
-#total 50GB,ingin membuat baru 30GB maka isi 20GB
-parted /dev/vda ---pretend-input-tty <<EOF
-resizepart 2 20GB
-yes
-30GB
-yes
-EOF
-clear
-
-
-#resize/mengecilkan partisi
+# Total 50GB,ingin membuat baru 30GB maka isi 20GB
 parted /dev/vda ---pretend-input-tty <<EOF
 resizepart 2 20GB
 yes
 quit
 EOF
 
-#membuat partisi baru
+# Buat partisi baru dari sisa ruang (20GB - 100%)
 parted /dev/vda --script mkpart primary btrfs 20GB 100%
-#refresh ulang sistem agar mengenali partisi baru
-partprobe /dev/vda
-#cek lsblk akan muncul partisi baru /dev/vda3
 
-#format partisi
+# Refresh partisi agar dikenali sistem
+partprobe /dev/vda
+sleep 2
+
+# Cek apakah partisi baru ada
+[ -b /dev/vda3 ] || { echo "Partisi /dev/vda3 tidak ditemukan!"; exit 1; }
+
+# Format ke Btrfs dengan label "dataku"
 mkfs.btrfs -f -L dataku /dev/vda3
+
+# Tampilkan info partisi
+lsblk -f
